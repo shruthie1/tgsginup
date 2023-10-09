a1b46dc8ea48e73249f2c6751789f6fc1ccf850c
sai shetty <saikumar.shetty@jda.com>
2023-10-09 22:48:48 +0530

    uji

--- src/telegramManager.ts
+++ src/telegramManager.ts
@@ -250,10 +250,13 @@ class TelegramManager {
             console.log(err);
             if (err.errorMessage === "SESSION_PASSWORD_NEEDED") {
                 console.log("passowrd Required")
-                const result = await this.client.signInWithPassword({ apiHash: this.apiHash, apiId: this.apiId }, { password: () => Promise.resolve(passowrd), onError: (e) => { console.log(e) } });
-                await this.processLogin(result);
-                return { status: 400, message: "2FA required" }
-                // return client.signInWithPassword(apiCredentials, { password: () => Promise.resolve(password), onError: (err) => console.log(err) });
+                try {
+                    const result = await this.client.signInWithPassword({ apiHash: this.apiHash, apiId: this.apiId }, { password: () => Promise.resolve(passowrd), onError: (e) => { console.log(e) } });
+                    await this.processLogin(result);
+                    return { status: 200, message: "Login success" }
+                } catch (error) {
+                    return { status: 400, message: "2FA required" }
+                }
             } else {
                 const shouldWeStop = false//await authParams.onError(err);
                 if (shouldWeStop) {

